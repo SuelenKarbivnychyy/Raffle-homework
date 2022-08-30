38e82cc6e605ecbc4876ef4d5510f8a0bdad037e
suelenk <suelenmatosr@gmail.com>
2022-08-30 16:34:45 -0400

    fixed code

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,50 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+from customers import Customer
+import random
+
+def get_customers_from_file(customer_file_path):
+    """Read customer file and return list of customer objects.
+
+    Read file at customer_file_path and create a customer object
+    containing customer information.
+    """
+
+    customers = []
+
+    customer_file = open(customer_file_path)
+
+    # Process a file like:
+    #
+    #   customer-name | email | street | city | zipcode
+
+    for line in customer_file:
+        customer_data = line.strip().split("|")
+        name, email, street, city, zipcode = customer_data
+
+        new_customer = Customer(name, email, street, city, zipcode)
+        customers.append(new_customer)
+
+    return customers
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")    
+    pick_winner(customers)
+
+run_raffle()
